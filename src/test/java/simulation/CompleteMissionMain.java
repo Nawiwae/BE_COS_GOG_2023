85692e0c003b817de6b3ce57c544f063f789d28e
Valentine Bourgeois <71726666+ValentineBrgs@users.noreply.github.com>
2023-10-23 13:28:54 +0200

    Update CompleteMissionMain.java

--- src/test/java/simulation/CompleteMissionMain.java
+++ src/test/java/simulation/CompleteMissionMain.java
@@ -42,36 +42,36 @@ public class CompleteMissionMain {
 		Map<Site, Timeline> accessPlan = mission.computeAccessPlan();
 		LOGGER.info(accessPlan.toString());
 
-		// Then we compute the observation plan, that is to say we fill a plan with
-		// Observation objects that can be achieved one after each other by the
-		// satellite without breaking the cinematic constraints imposed by the
-		// satellite agility.
-		Map<Site, AttitudeLawLeg> observationPlan = mission.computeObservationPlan();
-		LOGGER.info(observationPlan.toString());
+		// // Then we compute the observation plan, that is to say we fill a plan with
+		// // Observation objects that can be achieved one after each other by the
+		// // satellite without breaking the cinematic constraints imposed by the
+		// // satellite agility.
+		// Map<Site, AttitudeLawLeg> observationPlan = mission.computeObservationPlan();
+		// LOGGER.info(observationPlan.toString());
 
-		// Then, we compute the cinematic plan, which is the whole cinematic sequence of
-		// attitude law legs for our satellite during the mission horizon
-		StrictAttitudeLegsSequence<AttitudeLeg> cinematicPlan = mission.computeCinematicPlan();
-		LOGGER.info(cinematicPlan.toPrettyString());
+		// // Then, we compute the cinematic plan, which is the whole cinematic sequence of
+		// // attitude law legs for our satellite during the mission horizon
+		// StrictAttitudeLegsSequence<AttitudeLeg> cinematicPlan = mission.computeCinematicPlan();
+		// LOGGER.info(cinematicPlan.toPrettyString());
 
-		// Checking our cinematic plan
-		boolean validity = mission.checkCinematicPlan(cinematicPlan);
-		LOGGER.info("Plan validity : " + validity);
+		// // Checking our cinematic plan
+		// boolean validity = mission.checkCinematicPlan(cinematicPlan);
+		// LOGGER.info("Plan validity : " + validity);
 		
-		// Only if the cinematic plan is valid : we compute the score of our
-		// observationPlan
-		LOGGER.info("Number of cities visited : "+observationPlan.size());
-		double beforeComputingScore = System.currentTimeMillis();
-		LOGGER.info("Final score :"+ mission.computeFinalScore(observationPlan));
-		double afterComputingScore = System.currentTimeMillis();
-		LOGGER.info("Duration of the score computation : "+ 0.001 * (afterComputingScore - beforeComputingScore));
+		// // Only if the cinematic plan is valid : we compute the score of our
+		// // observationPlan
+		// LOGGER.info("Number of cities visited : "+observationPlan.size());
+		// double beforeComputingScore = System.currentTimeMillis();
+		// LOGGER.info("Final score :"+ mission.computeFinalScore(observationPlan));
+		// double afterComputingScore = System.currentTimeMillis();
+		// LOGGER.info("Duration of the score computation : "+ 0.001 * (afterComputingScore - beforeComputingScore));
 		
-		// Computing the time of execution
-		double t1 = System.currentTimeMillis();
-		LOGGER.info("Total duration : " + 0.001 * (t1 - t0));
+		// // Computing the time of execution
+		// double t1 = System.currentTimeMillis();
+		// LOGGER.info("Total duration : " + 0.001 * (t1 - t0));
 
-		// Finally, we write the VTS outputs to visualize and validate our plan
-		mission.generateVTSVisualization(cinematicPlan);
+		// // Finally, we write the VTS outputs to visualize and validate our plan
+		// mission.generateVTSVisualization(cinematicPlan);
 
 		LOGGER.info("\n\nSimulation done");
 
